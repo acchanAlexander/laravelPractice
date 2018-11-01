83e33f7b458479320e178928debaafdcf1160f5c
acchanAlexander <xxxg_01_w_endless_waltz@yahoo.co.jp>
2018-11-01 14:49:18 +0000

    /tasks で validation 処理の追加

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,15 @@ Route::get('/', function () {
 });
 
 Route::post('/task', function (Request $request) {
-    //
+  $validator = Validator::make($request->all(), [
+    'name' => 'required|max:255',
+  ]);
+
+  if ($validator->fails()) {
+    return redirect('/')
+      ->withInput()
+      ->withErrors($validator);
+  }
 });
 
 Route::delete('/task/{id}', function ($id) {
