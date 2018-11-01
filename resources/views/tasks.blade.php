fabca45eef6b2ce0c9dd468c501e8270dc0a3f7c
acchanAlexander <xxxg_01_w_endless_waltz@yahoo.co.jp>
2018-11-01 15:11:10 +0000

    削除ボタンを追加

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -59,6 +59,14 @@
 
                                 <td>
                                     <!-- TODO: 削除ボタン -->
+                                    <td>
+                                      <form action="/task/{{ $task->id }}" method="POST">
+                                          {{ csrf_field() }}
+                                          {{ method_field('DELETE') }}
+
+                                          <button>タスク削除</button>
+                                      </form>
+                                  </td>
                                 </td>
                             </tr>
                         @endforeach
